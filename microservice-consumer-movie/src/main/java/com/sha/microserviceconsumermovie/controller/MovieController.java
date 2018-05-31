76bc84951cec815ddef7c1464595f32cc7a27830
rong_eclipse <rongmazhong@outlook.com>
2018-05-31 23:25:53 +0800

    添加 ribben 负载均衡

--- microservice-consumer-movie/src/main/java/com/sha/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/sha/microserviceconsumermovie/controller/MovieController.java
@@ -1,8 +1,9 @@
 package com.sha.microserviceconsumermovie.controller;
 
 import com.sha.microserviceconsumermovie.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private RestTemplate restTemplate;
 
